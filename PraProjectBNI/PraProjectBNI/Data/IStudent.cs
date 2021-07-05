0c52d45ec310c17a64c4dc1073ac604f303d9fa5
Dhea Laksmi Prianto <86338843+dhealaksmi@users.noreply.github.com>
2021-07-05 23:04:03 +0700

    menambahkan api student

---
+++ PraProjectBNI/PraProjectBNI/Data/IStudent.cs
@@ -0,0 +1,22 @@
+﻿using PraProjectBNI.Models;
+using SampleAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraProjectBNI.Data
+{
+    public interface IStudent : ICrud<Student>
+    {
+        IEnumerable<Student> GetByName(string Student);
+        Student GetById(int id);
+        new void Insert(Student student);
+        void Update(int id, Student student);
+        void Delete(int id);
+        IEnumerable<Student> GetByName();
+        object InsertWithIndentity(Student student);
+        new void Update(string v, Student student);
+        new void Delete(string v);
+    }
+}
