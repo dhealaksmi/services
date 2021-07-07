1d582a5cca594790932ff79e4181dca2f1fc65e8
Dhea Laksmi Prianto <86338843+dhealaksmi@users.noreply.github.com>
2021-07-07 09:08:07 +0700

    update student

--- PraProjectBNI/PraProjectBNI/Models/Student.cs
+++ PraProjectBNI/PraProjectBNI/Models/Student.cs
@@ -1,23 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 #nullable disable
 
 namespace PraProjectBNI.Models
 {
-    public partial class Student
+    public class Student
     {
-        public Student()
-        {
-            Enrollments = new HashSet<Enrollment>();
-        }
-
-        public int IdStudent { get; set; }
+        public int ID_Student { get; set; }
         public string Nama { get; set; }
         public string Domisili { get; set; }
         public int? Angkatan { get; set; }
-        public string JenisKelamin { get; set; }
+        public string Jenis_Kelamin { get; set; }
+        public object Enrollments { get; internal set; }
+        internal void Update(string v, Student student)
+        {
+            throw new NotImplementedException();
+        }
 
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        internal static void Delete(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object InsertWithIndentity(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Task GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Dosen GetByName(object Dosen, object Student)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Student GetByName(object Student)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Insert(Dosen dosen)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Insert(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object InsertWithIndentity(Dosen dosen)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
