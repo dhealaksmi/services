a68dc2fe62a1fa31d27c0a8dba8152a7becffed7
Dhea Laksmi Prianto <86338843+dhealaksmi@users.noreply.github.com>
2021-07-07 09:06:30 +0700

    update istudent

--- PraProjectBNI/PraProjectBNI/Data/IStudent.cs
+++ PraProjectBNI/PraProjectBNI/Data/IStudent.cs
@@ -1,22 +1,73 @@
-﻿using PraProjectBNI.Models;
-using SampleAPI.Data;
+using PraProjectBNI.Models;
+using PraProjectBNI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace PraProjectBNI.Data
 {
-    public interface IStudent : ICrud<Student>
+    public class IStudent : ICrud<Student>
     {
-        IEnumerable<Student> GetByName(string Student);
-        Student GetById(int id);
-        new void Insert(Student student);
-        void Update(int id, Student student);
-        void Delete(int id);
-        IEnumerable<Student> GetByName();
-        object InsertWithIndentity(Student student);
-        new void Update(string v, Student student);
-        new void Delete(string v);
+        private PraBNIContext _db;
+
+        public async Task<IEnumerable<IStudent>> GetAll()
+        {
+            var results = await (from s in _db.Student
+                                 orderby s.Nama
+                                 select s).ToListAsync();
+            return (IEnumerable<IStudent>)results;
+        }
+
+        public async Task<Student> GetById(string id)
+        {
+            var result = await (from s in _db.Student
+                                orderby s.ID_Student == Convert.ToInt32(id) select s)
+                                .FirstOrDefaultAsync();
+            return result;
+        }
+
+        public async Task Insert(Student obj)
+        {
+            try
+            {
+                _db.Student.Add(obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
+        }
+
+        public async Task Delete(string id)
+        {
+            var result = await GetById(id);
+            if (result != null)
+            {
+                try
+                {
+                    _db.Student.Remove(result);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateException dbEx)
+                {
+                    throw new Exception($"DbError: {dbEx.Message}");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        Task<IEnumerable<Student>> ICrud<Student>.All => throw new NotImplementedException();
+
+        public Task Update(string id, Student obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
